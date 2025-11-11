8871e7bcaadaa8319871163d85e65cb7c7748600
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-11-12 08:47:22 +0900

    ==DELETE POSTS==

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import { HomePage } from './renderer/pages/home';
 import { NotFoundPage } from './renderer/pages/404';
 import { jsxRenderer } from 'hono/jsx-renderer';
 
-//import { postsPage } from './renderer/pages/posts'
-
 // Define the bindings available to our worker
 type Bindings = {
 	ASSETS: Fetcher;
@@ -25,31 +23,6 @@ app.use(trimTrailingSlash());
 app.get('/', (c) => {
 	return c.render(<HomePage />);
 });
-
-/*app.get('/posts', async (c) => {
-  try {
-    const res = await fetch('https://dev.to/api/articles?username=nyanchu_okabe_b2a95eb4beb', {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    })
-    if (!res.ok) {
-      console.error(`API request failed with status: ${res.status}`);
-      const errorText = await res.text();
-      console.error(`API response: ${errorText}`);
-      return c.render(<postsPage posts={[]} />)
-    }
-    const posts = await res.json()
-    const simplified = posts.map((p: any) => ({
-      title: p.title,
-      url: p.url,
-    }))
-    return c.render(<postsPage posts={simplified} />)
-  } catch (err) {
-    console.error('Error fetching posts:', err);
-    return c.render(<postsPage posts={[]} />)
-  }
-  })*/
 
 // --- Dynamic Page Route ---
 app.get('/:page', async (c) => {
