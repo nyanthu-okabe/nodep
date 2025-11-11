e5a3c51738dd70bdee96662b4cab83ff7361fef2
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-11-12 07:50:29 +0900

    Update posts.tsx

--- src/renderer/pages/posts.tsx
+++ src/renderer/pages/posts.tsx
@@ -1,28 +1,13 @@
 import { FC } from 'hono/jsx'
 
-// 取得関数
-async function fetchTitles() {
-  const res = await fetch('https://dev.to/api/articles?username=nyanchu_okabe_b2a95eb4beb')
-  if (!res.ok) throw new Error('Failed to fetch posts')
-  const posts = await res.json()
-  return posts.map((p: any) => ({
-    title: p.title,
-    url: p.url
-  }))
-}
-
-// JSXコンポーネント
-export const postsPage: FC = async () => {
-  const posts = await fetchTitles()
-
-  return (
-    <section id="posts">
-      <h2>All posts</h2>
-        {posts.map((p) => (
-          <a href={p.url} target="_blank" rel="noopener noreferrer">
-            {p.title}
-          </a>
-        ))}
-    </section>
-  )
-}
+// ページコンポーネント（同期）
+export const postPage: FC<{ posts: { title: string; url: string }[] }> = ({ posts }) => (
+  <section id="posts">
+    <h2>All posts</h2>
+      {posts.map((p) => (
+				<a href={p.url} target="_blank" rel="noopener noreferrer">
+					{p.title}
+				</a>
+      ))}
+  </section>
+)
