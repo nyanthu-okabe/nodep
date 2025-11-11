eadb630321f0fa8a6629a92faa12f515466684b2
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-11-12 07:56:56 +0900

    Update posts.tsx

--- src/renderer/pages/posts.tsx
+++ src/renderer/pages/posts.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'hono/jsx'
 
 // ページコンポーネント（同期）
-export const postPage: FC<{ posts: { title: string; url: string }[] }> = ({ posts }) => (
+export const postsPage: FC<{ posts: { title: string; url: string }[] }> = ({ posts }) => (
   <section id="posts">
     <h2>All posts</h2>
       {posts.map((p) => (
-				<a href={p.url} target="_blank" rel="noopener noreferrer">
-					{p.title}
-				</a>
+		<a href={p.url} target="_blank" rel="noopener noreferrer">
+			{p.title}
+		</a>
       ))}
   </section>
 )
