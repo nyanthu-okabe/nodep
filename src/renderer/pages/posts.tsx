fe149cc5e051ef827f0eff4530ae8d818db0c752
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-11-12 07:41:20 +0900

    Create posts.tsx

---
+++ src/renderer/pages/posts.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'hono/jsx'
+
+// 取得関数
+async function fetchTitles() {
+  const res = await fetch('https://dev.to/api/articles?username=user123')
+  if (!res.ok) throw new Error('Failed to fetch posts')
+  const posts = await res.json()
+  return posts.map((p: any) => ({
+    title: p.title,
+    url: p.url
+  }))
+}
+
+// JSXコンポーネント
+export const postsPage: FC = async () => {
+  const posts = await fetchTitles()
+
+  return (
+    <section id="posts">
+      <h2>All posts</h2>
+        {posts.map((p) => (
+          <a href={p.url} target="_blank" rel="noopener noreferrer">
+            {p.title}
+          </a>
+        ))}
+      </ul>
+    </section>
+  )
+}
